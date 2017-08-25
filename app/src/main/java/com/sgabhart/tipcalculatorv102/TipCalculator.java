ea9d5e4bfee96bedf87c780ad32fc77d54139619
Spencer Gabhart <sgabhart2230@gmail.com>
2017-08-25 09:44:28 -0400

    First commit on new version

---
+++ app/src/main/java/com/sgabhart/tipcalculatorv102/TipCalculator.java
@@ -0,0 +1,35 @@
+package com.sgabhart.tipcalculatorv102;
+
+
+public class TipCalculator {
+    private float tip, bill;
+
+    public TipCalculator(float newTip, float newBill) {
+        setTip(newTip);
+        setBill(newBill);
+    }
+
+    public float getTip() {
+        return tip;
+    }
+
+    public float getBill() {
+        return bill;
+    }
+
+    public void setTip(float newTip) {
+        if(newTip > 0) this.tip = newTip;
+    }
+
+    public void setBill(float newBill) {
+        if(newBill > 0) this.bill = newBill;
+    }
+
+    public float tipAmount() {
+        return bill * tip;
+    }
+
+    public float totalAmount() {
+        return bill + tipAmount();
+    }
+}
