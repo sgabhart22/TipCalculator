91442fb38a23dd75c09ab29ee4e20e304d1c8879
Spencer Gabhart <sgabhart2230@gmail.com>
2017-08-28 09:10:45 -0400

    Removed button, added listener

--- app/src/main/java/com/sgabhart/tipcalculatorv4/MainActivity.java
+++ app/src/main/java/com/sgabhart/tipcalculatorv4/MainActivity.java
@@ -7,23 +7,31 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.text.NumberFormat;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 public class MainActivity extends AppCompatActivity {
 
     private TipCalculator tc;
     public NumberFormat money = NumberFormat.getCurrencyInstance();
+    private EditText billEdit;
+    private EditText tipEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tc = new TipCalculator(0.17f, 100.0f);
         setContentView(R.layout.activity_main);
+
+        billEdit = (EditText)(findViewById(R.id.amount_bill));
+        tipEdit = (EditText)(findViewById(R.id.amount_tip_percent));
+
+        TextChangeHandler tch = new TextChangeHandler();
+        billEdit.addTextChangedListener(tch);
+        tipEdit.addTextChangedListener(tch);
     }
 
-    public void calculate(View view) {
-        Log.w("MainActivity", "view = " + view);
-        EditText billEdit = (EditText)(findViewById(R.id.amount_bill));
-        EditText tipEdit = (EditText)(findViewById(R.id.amount_tip_percent));
+    public void calculate() {
         String billStr = billEdit.getText().toString();
         String tipStr = tipEdit.getText().toString();
 
@@ -43,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
             tipText.setText(money.format(tip));
             totalText.setText(money.format(total));
         } catch (NumberFormatException e) {
+
+        }
+    }
+
+    private class TextChangeHandler implements TextWatcher {
+        public void afterTextChanged(Editable e){
+            calculate();
+        }
+
+        public void beforeTextChanged(CharSequence cs, int start, int count, int after){
+
+        }
+
+        public void onTextChanged(CharSequence cs, int start, int before, int after){
 
         }
     }
